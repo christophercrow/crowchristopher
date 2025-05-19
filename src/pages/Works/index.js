758b6938e61d8635fe1a9c35076ad0563badada8
Christopher Crow <cecrow@unc.edu>
2025-05-18 22:29:47 -0400

    Refactor: updated Works page imports and structure for new project layout

--- src/pages/Works/index.js
+++ src/pages/Works/index.js
@@ -1,19 +1,27 @@
+// Refactored Works Page Imports
+
 import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
-// import { Link } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
-import DesktopNav from "../../components/DesktopNav"
-import favicon from '../../images/favicon.ico'
-import appleIcon from '../../images/apple-touch-icon.png'
-import favicon32 from '../../images/favicon-32x32.png'
-import favicon16 from '../../images/favicon-16x16.png'
-import siteManifest from '../../images/site.webmanifest'
-import Cursor from "../../components/Cursor";
-import SocialIcons from "../../components/SocialIcons";
-import { useTrail, a, useChain, useSpring, useSpringRef, useTransition } from '@react-spring/web'
-import Search from "../../components/Search";
-import winkConfig from "../../config.json";
-import WorksCard from "../../components/WorksCard";
+
+// Updated component imports
+import DesktopNav from "../../components/layout/DesktopNav";
+import Cursor from "../../components/ui/Cursor";
+import SocialIcons from "../../components/ui/SocialIcons";
+import Search from "../../components/features/Search";
+import WorksCard from "../../components/features/WorksCard";
+
+// Updated metadata and asset paths
+import favicon from '../../assets/images/favicon.ico';
+import appleIcon from '../../assets/images/apple-touch-icon.png';
+import favicon32 from '../../assets/images/favicon-32x32.png';
+import favicon16 from '../../assets/images/favicon-16x16.png';
+import siteManifest from '../../assets/images/site.webmanifest';
+
+import { useTrail, a, useChain, useSpring, useSpringRef, useTransition } from '@react-spring/web';
+
+// Updated config path
+import winkConfig from "../../data/config.json";
 
 function getWindowSize() {
     const {innerWidth, innerHeight} = window;
